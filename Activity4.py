12cb5c8d9b6661d5eb3bd6bf6a76f62ecd30bbe5
SydneyFlores <mharsydney@gmail.com>
2022-06-30 14:01:54 +0800

    2.8 Coding Exercises

---
+++ Activity4.py
@@ -0,0 +1,19 @@
+#Instruction 1
+groceries = {"chicken": 8, "apples": 6, "cucumbers": 3, "milk": 2, "oranges": 4}
+
+cart = groceries.pop("oranges")
+print(groceries)
+
+#Instruction 2
+speakers = {"Sir Rafael": 54, "Ms.Joan": 33, "Ms.Dana": 67}
+
+print(speakers.keys())
+
+#Intsruction 3
+Result_of_the_swimming_tryout = {"Carl": "passed", "Quentin": "failed", 
+"Johhn Y.": "passed", "Peter": "failed", "Max T": "passed", 
+"Joesph": "passed", "Jone": "failed", "Jorge": "failed", 
+"George": "passed", "Ben": "passsed", "Jerome": "passed", "Rick": "failed", 
+"Max G": "failed", "John P.": "failed", "Vince": "passed"}
+
+print(Result_of_the_swimming_tryout.get("Jorge"))
